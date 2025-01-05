f5989c60ffe3a19d5cd82e9d3ad350a5ea63c39c
Your Name <sara188171@gmail.com>
2025-01-05 22:07:28 +0200

    ProductCatalog

---
+++ ProductCatalog/app/app.controller.js
@@ -0,0 +1,26 @@
+﻿angular.module('productCatalogApp')
+    .controller('productListController', function ($scope, productService) {
+        $scope.products = [];
+        $scope.loadProducts = function () {
+            productService.getProducts().then(function (response) {
+                $scope.products = response.data;
+            });
+        };
+         $scope.addProduct = function () {
+            var newProduct = {
+                name: $scope.productName,
+                category: $scope.productCategory,
+                price: $scope.productPrice
+            };
+
+             productService.addProduct(newProduct).then(function (response) {
+                 $scope.products.push(response.data);
+            });
+
+             $scope.productName = '';
+            $scope.productCategory = '';
+            $scope.productPrice = '';
+        };
+        $scope.loadProducts();
+
+     });
---
+++ ProductCatalog/app/app.service.js
@@ -0,0 +1,12 @@
+﻿angular.module('productCatalogApp')
+    .service('productService', ['$http', function ($http) {
+        var apiUrl = 'https://localhost:7055/api/products';
+
+        this.getProducts = function () {
+            return $http.get(apiUrl);
+        };
+
+        this.addProduct = function (product) {
+            return $http.post(apiUrl, product);
+        };
+    }]);
